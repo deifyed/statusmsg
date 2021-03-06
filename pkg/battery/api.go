7a5ec18c176617159d324d60671cbff520161814
Julius Pedersen <deifyed@tutanota.com>
2021-03-06 17:34:05 +0100

    👌 Rename GetBatteryStatus() for idiomaticy

--- pkg/battery/api.go
+++ pkg/battery/api.go
@@ -14,23 +14,23 @@ var defaultBatteryStatusPath = path.Join(defaultBatteryPath, "status")
 
 func (b Status) String() string {
 	icon := ""
-	
+
 	switch strings.ToLower(b.Status) {
 	case "discharging":
 		icon = "🔋"
 	default:
 		icon = "⚡ "
 	}
-	
+
 	return fmt.Sprintf("%s %s%s", icon, b.Capacity, "%")
 }
 
-func GetBatteryStatus() (Status, error) {
+func GetStatus() (Status, error) {
 	rawCapacity, err := os.ReadFile(defaultBatteryCapacityPath)
 	if err != nil {
-	    return Status{}, fmt.Errorf("reading capacity: %w", err)
+		return Status{}, fmt.Errorf("reading capacity: %w", err)
 	}
-	
+
 	rawStatus, err := os.ReadFile(defaultBatteryStatusPath)
 	if err != nil {
 		return Status{}, fmt.Errorf("reading status: %w", err)
